79260c23998d8f3596c32d14b0278412d90bad03
pikachu1509 <bhagvatrahul0915@gmail.com>
2024-07-23 10:22:39 +0530

    try to modifed

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Item.css';
 const Item = (props) => {
   return (
+    
     <div className='item'>
       <img src={props.image} alt="" />
       <p>{props.name}</p>
@@ -13,6 +14,7 @@ const Item = (props) => {
             ₹{props.old_price}
             </div>
       </div>
+      
     </div>
   )
 }
