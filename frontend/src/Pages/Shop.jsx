a1deb1de5fe55e679851f7669c87d69c9435bb7f
Prakrutiparesh <pareshchapatwala91993@gmail.com>
2024-07-23 22:05:36 +0530

    complete shop page

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
+import NewCollections from '../Components/NewCollections/NewCollections'
+import NewsLetter from '../Components/NewsLatter/NewsLetter'
+
 
 export default function Shop() {
   return (
@@ -9,6 +12,9 @@ export default function Shop() {
     <Hero/>
     <Popular/>
     <Offers/>
+    <NewCollections/> 
+    <NewsLetter/>
+   
     </div>
   )
 }
