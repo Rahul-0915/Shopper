3e8c2bd40b7368e678bafd02ac2567812a9c23a4
Prakrutiparesh <pareshchapatwala91993@gmail.com>
2024-07-24 17:26:16 +0530

    chnage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,11 @@ import LoginSignup from './Pages/LoginSignup';
 import Shop from './Pages/Shop';
 import Cart from './Pages/Cart';
 import Footer from './Components/Footer/Footer';
+// banners
+import men_banner from './Components/Assets/banner_mens.png';
+import Women_baneer from './Components/Assets/banner_women.png';
+import kid_banner from './Components/Assets/banner_kids.png';
+
 
 function App() {
   return (
@@ -17,9 +22,9 @@ function App() {
         <Navbar />
         <Routes>
         <Route path='/' element={<Shop/>}/>
-          <Route path='/Mens' element={<ShopCategory Category="Men" />}/>
-          <Route path='/Womens' element={<ShopCategory Category="Women" />}/>
-          <Route path='/Kids' element={<ShopCategory Category="Kids" />}/>
+          <Route path='/Mens' element={<ShopCategory banner={men_banner} Category="Men" />}/>
+          <Route path='/Womens' element={<ShopCategory banner={Women_baneer} Category="Women" />}/>
+          <Route path='/Kids' element={<ShopCategory banner={kid_banner} Category="Kids" />}/>
           <Route path='/Product' element={<Product/>}/>
           <Route path=':productId' element={<Product/>}/>
         <Route/>
