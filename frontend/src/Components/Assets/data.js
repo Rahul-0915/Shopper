756dd52fe59bf8d68f5702539e557df5eb6bca80
Prakrutiparesh <pareshchapatwala91993@gmail.com>
2024-07-24 14:50:07 +0530

    change images

--- frontend/src/Components/Assets/data.js
+++ frontend/src/Components/Assets/data.js
@@ -1,4 +1,4 @@
-import p1_img from './product_1.png'
+import p1_img from './product_1.png';
 import p2_img from './product_2.png'
 import p3_img from './product_3.png'
 import p4_img from './product_4.png'
@@ -6,28 +6,28 @@ import p4_img from './product_4.png'
 let data_product = [
   {
     id:1,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name:"Uptownie Lite Casual Sleeveless Solid Women White Top",
     image:p1_img,
-    new_price:50.00,
-    old_price:80.50,
+    new_price:"329",
+    old_price:"1,599",
   },
   {id:2,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name:"RYLEE Casual Regular Sleeves Printed Women Dark Blue Top",
     image:p2_img,
-    new_price:85.00,
-    old_price:120.50,
+    new_price:"295",
+    old_price:"1,399",
   },
   {id:3,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name:"swaggish Casual Regular Sleeves Solid Women Brown Top",
     image:p3_img,
-    new_price:60.00,
-    old_price:100.50,
+    new_price:"229",
+    old_price:"1,499",
   },
   {id:4,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name:"Dressberry Casual Cuffed Sleeves Graphic Print Women Blue Top",
     image:p4_img,
-    new_price:100.00,
-    old_price:150.00,
+    new_price:"770",
+    old_price:"1,399",
   },
 ];
 
