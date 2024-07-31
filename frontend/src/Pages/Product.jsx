18642e422cab519a98954eb972cb5ab2ffb3a1cf
pikachu1509 <bhagvatrahul0915@gmail.com>
2024-07-31 10:44:35 +0530

    craete drescrition box and realted product

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -4,6 +4,8 @@ import { ShopContext } from '../Context/ShopContext';
 import { useParams } from 'react-router-dom';
 import Breadcrum from '../Components/Breadcrums/Breadcrum';
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
 // import Navbar from '../Components/Navbar/Navbar';
 // import Footer from '../Components/Footer/Footer';
 const Product = () => {
@@ -14,6 +16,10 @@ const Product = () => {
     <div>
       <Breadcrum product={product} />
       <ProductDisplay product={product} />
+
+      {/* use for prdouct description brif... */}
+      <DescriptionBox />
+      <RelatedProducts />
     </div>
   )
 }
