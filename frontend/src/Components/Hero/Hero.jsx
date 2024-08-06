40ba08e519b30699a02f945b417a8d0cc9900b6e
pikachu1509 <bhagvatrahul0915@gmail.com>
2024-08-06 15:03:27 +0530

    final home page css:

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,32 +1,42 @@
 import React from 'react'
 import './Hero.css';
-import hand_icon from '../Assets/hand_icon.png'
-import arrow_icon from '../Assets/arrow.png'
-import hero_image from '../Assets/hero_section2.png'
+// import hand_icon from '../Assets/hand_icon.png'
+// import arrow_icon from '../Assets/arrow.png'
+// import hero_image from '../Assets/hero_section2.png'
+import hero_banner from '../Assets/hero_banner.png'
 
 const Hero = () => {
-  return (
-    <div className="hero">
-      <div className="hero-left">
-            <h2>NEW ARRIVALS ONLY</h2>
-            <div>
-                  <div className="hero-hand-icon">
-                        <p>new</p>
-                        <img src={hand_icon} alt="" />
-                  </div>
-                  <p>Collections</p>
-                  <p>For Everyone</p>
+
+      return (
+            <div className="heronew">
+                  <img src={hero_banner} alt="" />
+                  
             </div>
-            <div className="hero-latest-btn">
-                  <div>Latest Collection</div>
-                  <img src={arrow_icon} alt="" />
-            </div>
-      </div>
-      <div className="hero-right">
-            <img src={hero_image} alt="" />
-      </div>
-    </div>
-  )
+      ) 
 }
+//   return (
+
+//     <div className="hero">
+//       <div className="hero-left">
+//             <h2>NEW ARRIVALS ONLY</h2>
+//             <div>
+//                   <div className="hero-hand-icon">
+//                         <p>new</p>
+//                         <img src={hand_icon} alt="" />
+//                   </div>
+//                   <p>Collections</p>
+//                   <p>For Everyone</p>
+//             </div>
+//             <div className="hero-latest-btn">
+//                   <div>Latest Collection</div>
+//                   <img src={arrow_icon} alt="" />
+//             </div>
+//       </div>
+//       <div className="hero-right">
+//             <img src={hero_image} alt="" />
+//       </div>
+//     </div>
+//   )
+// }
 
 export default Hero
--- frontend/src/Components/Offers/Offers.jsx
+++ frontend/src/Components/Offers/Offers.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import './Offers.css';
-import exclusive_image from '../Assets/hero_section.png';
+// import exclusive_image from '../Assets/hero_section.png';
+import ex_offer from '../Assets/Ex_offer.png';
 
-const Offers = () => {
+const Offers = () => 
+{
   return (
-    <div className='offers'>
-      <div className="offers-left">
-            <h1>Exclusive</h1>
-            <h1>Offers For You</h1>
-            <p>ONLY ON BEST SELLERS PRODUCT</p>
-            <button>Check Now</button>
-      </div>
-      <div className="offers-right">
-            <img src={exclusive_image} alt="" />
-      </div>
+    <div className="offerdiv">
+      <img src={ex_offer} alt="" />
     </div>
   )
 }
+//   {
+//   return (
+//     <div className='offers'>
+//       <div className="offers-left">
+//             <h1>Exclusive</h1>
+//             <h1>Offers For You</h1>
+//             <p>ONLY ON BEST SELLERS PRODUCT</p>
+//             <button>Check Now</button>
+//       </div>
+//       <div className="offers-right">
+//             <img src={exclusive_image} alt="" />
+//       </div>
+//     </div>
+//   )
+// }
 
 export default Offers
