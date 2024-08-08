86c7604746cb8ce8d266c4ec6b3a58145d0fffc1
Prakrutiparesh <pareshchapatwala91993@gmail.com>
2024-08-08 21:36:56 +0530

    admin backend by prakruti

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -2,26 +2,58 @@ import React, { useState } from 'react'
 import './AddProduct.css'
 import upload_area from '../../assets/upload_area.svg'
 
-function AddProduct() {
+const  AddProduct = ()=> {
 
     // for upload image..
     const [image, setImage] = useState(false);
+    const [productDetails,setProductDetails] = useState({
+        name:"",
+        image:"",
+        category:"Women",
+        new_price:"",
+        old_price:""
+    })
+
     const imageHandler = (e) => {
         setImage(e.target.files[0]);
     }
     // for add product details..
-    const [productDetails,setProductDetails]=useState({
-        name:"",
-        image:"",
-        category:"women",
-        new_price:"",
-        old_price:""
-    })
-    const changeHandler=(e)=>{
+    
+    const changeHandler = (e) =>{
         setProductDetails({...productDetails,[e.target.name]:e.target.value})
     }
-    const Add_Product= async ()=>{
+    const Add_Product = async ()=>{
         console.log(productDetails);
+        let responseData;
+        let product = productDetails;
+
+        let formData = new FormData();
+        formData.append('product',image);
+
+        await fetch('http://localhost:4000/upload',{
+            method:'POST',
+            headers:{
+                Accept:'application/json',
+            },
+            body:formData,
+        }).then((resp) => resp.json()).then((data)=>{responseData=data});
+
+        if(responseData.success)
+        {
+            product.image = responseData.image_url;
+            console.log(product);
+            await fetch('http://localhost:4000/addproduct',{
+                method:'POST',
+                headers:{
+                    Accept:'application/json',
+                   'Content-Type':'application/json',
+                },
+                body:JSON.stringify(product),
+            }).then((resp)=>resp.json()).then((data)=>{
+                data.success?alert("Product Added"):alert("Failed");
+            })
+        }
+
     }
     return (
         <div className='add-product'>
@@ -33,7 +65,7 @@ function AddProduct() {
                 <div className='addproduct-itemfield'>
                     <p>Price</p>
                     
-                    <input value={productDetails.Old_price} onChange={changeHandler} type="text" name="old_price" id="" placeholder='Type here..' />
+                    <input value={productDetails.old_price} onChange={changeHandler} type="text" name="old_price" id="" placeholder='Type here..' />
                 </div>
                 <div className='addproduct-itemfield'>
                     <p>Offer Price</p>
@@ -44,17 +76,17 @@ function AddProduct() {
                 <p>Product Category</p>
                 <select value={productDetails.category} onChange={changeHandler} name="category" className='add-product-selector'>
                     <option value="women">Women</option>
-                    <option value="men">men</option>
-                    <option value="kid">kid</option>
+                    <option value="men">Men</option>
+                    <option value="kid">Kid</option>
                 </select>
             </div>
             <div className='addproduct-itemfield'>
                 <label htmlFor="file-input">
-                    <img src={image ? URL.createObjectURL(image) : upload_area} alt="" className='addproduct-thumnail-img' />
+                    <img src={image?URL.createObjectURL(image) : upload_area} alt="" className='addproduct-thumnail-img' />
                 </label>
                 <input onChange={imageHandler} type="file" name="image" id="file-input" hidden />
             </div>
-            <button onClick={Add_Product} className='addproduct-btn'>ADD</button>
+            <button onClick={()=>{Add_Product()}} className='addproduct-btn'>ADD</button>
         </div>
 
     )
